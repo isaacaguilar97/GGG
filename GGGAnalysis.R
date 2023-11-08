c57fef37964f8b91059bdf439bf367c2e7ee06f4
Isaac Aguilar <oscarisaacaguilarembila@gmail.com>
2023-11-08 15:24:11 -0700

    Boosted trees

--- GGGAnalysis.R
+++ GGGAnalysis.R
@@ -1,8 +1,8 @@
-# library(doParallel)
-# 
-# num_cores <- parallel::detectCores() #How many cores do I have?
-# cl <- makePSOCKcluster(num_cores)
-# registerDoParallel(cl)
+library(doParallel)
+
+num_cores <- parallel::detectCores() #How many cores do I have?
+cl <- makePSOCKcluster(num_cores)
+registerDoParallel(cl)
 
 library(tidyverse)
 library(vroom)
@@ -10,6 +10,10 @@ library(tidymodels)
 library(naivebayes)
 library(discrim)
 library(DataExplorer)
+library(bonsai)
+library(lightgbm)
+library(embed)
+
 
 
 # Load the data -----------------------------------------------------------
@@ -23,7 +27,7 @@ testSet <- vroom('./test.csv')
 
 # # EDA ---------------------------------------------------------------------
 # 
-# dplyr::glimpse(trainSet)
+dplyr::glimpse(trainSet)
 # plot_missing(trainSet)
 # plot_correlation(trainSet)
 # plot_histogram(trainSet)
@@ -96,45 +100,104 @@ testSet <- vroom('./test.csv')
 
 # Neural Networks ---------------------------------------------------------
 
-nn_recipe <- recipe(type~., data=trainSet) %>%
-  step_dummy(all_nominal_predictors()) %>%
-  step_range(all_numeric_predictors(), min=0, max=1) #scale to [0,1]
-
-# nn_recipe <- recipe(formula=, data=) %>% 
-#   update_role(id, new_role="id") %>% 
-#   step_...() %>% ## Turn color to factor then dummy encode color
+# nn_recipe <- recipe(type~., data=trainSet) %>%
+#   step_dummy(all_nominal_predictors()) %>%
 #   step_range(all_numeric_predictors(), min=0, max=1) #scale to [0,1]
+# 
+# # nn_recipe <- recipe(formula=, data=) %>% 
+# #   update_role(id, new_role="id") %>% 
+# #   step_...() %>% ## Turn color to factor then dummy encode color
+# #   step_range(all_numeric_predictors(), min=0, max=1) #scale to [0,1]
+# 
+# nn_model <- mlp(hidden_units = tune(),
+#                 epochs = 50 #or 100 or 250
+#                 ) %>%
+#   set_engine("nnet") %>% #verbose = 0 prints off less (or nnet)
+#   set_mode("classification")
+# 
+# # Workflow
+# nn_wf <- workflow() %>%
+#   add_recipe(nn_recipe) %>%
+#   add_model(nn_model)
+# 
+# # Tune
+# nn_tuneGrid <- grid_regular(hidden_units(range=c(1, 10)),
+#                             levels=10)
+# 
+# ## Set up K-fold CV
+# folds <- vfold_cv(trainSet, v = 10, repeats=1)
+# 
+# # Cross Validation
+# tuned_nn <- nn_wf %>%
+#   tune_grid(resamples=folds,
+#             grid=nn_tuneGrid,
+#             metrics=metric_set(accuracy))
+# 
+# ## Find best tuning parameters
+# bestTune <- tuned_nn  %>%
+#   select_best("accuracy")
+# 
+# # Finalize Workflow
+# final_wf <- nn_wf %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=trainSet)
+# 
+# ## Predict
+# predictions <- final_wf %>%
+#   predict(new_data = testSet, type = "class")
+# 
+# # Format table
+# testSet$type <- predictions$.pred_class
+# results <- testSet %>%
+#   select(id, type)
+# 
+# # get csv file
+# vroom_write(results, 'GGGPredsnn.csv', delim = ",")
+# 
+# # Plot graph
+# graph <- tuned_nn %>% collect_metrics() %>%
+#   filter(.metric=="accuracy") %>%
+#   ggplot(aes(x=hidden_units, y=mean)) + geom_line()
+# graph
+# save(file="./MyFile.RData", list=c("predictions", "tuned_nn"))
 
-nn_model <- mlp(hidden_units = tune(),
-                epochs = 50 #or 100 or 250
-                ) %>%
-  set_engine("nnet") %>% #verbose = 0 prints off less (or nnet)
+
+# BOOST TREES -------------------------------------------------------------
+bt_recipe <- recipe(type~., data=trainSet) %>%
+  step_dummy(all_nominal_predictors())
+
+boost_model <- boost_tree(tree_depth=tune(),
+                          trees=tune(),
+                          learn_rate=tune()) %>%
+  set_engine("lightgbm") %>% #or "xgboost" but lightgbm is faster
   set_mode("classification")
 
 # Workflow
-nn_wf <- workflow() %>%
-  add_recipe(nn_recipe) %>%
-  add_model(nn_model)
+bt_wf <- workflow() %>%
+  add_recipe(bt_recipe) %>%
+  add_model(boost_model)
 
 # Tune
-nn_tuneGrid <- grid_regular(hidden_units(range=c(1, 10)),
-                            levels=10)
+tuneGrid <- grid_regular(tree_depth(), 
+                            trees(),
+                            learn_rate(),
+                            levels = 5)
 
 ## Set up K-fold CV
-folds <- vfold_cv(trainSet, v = 10, repeats=1)
+folds <- vfold_cv(trainSet, v = 5, repeats=1)
 
 # Cross Validation
-tuned_nn <- nn_wf %>%
+tuned_bt <- bt_wf %>%
   tune_grid(resamples=folds,
-            grid=nn_tuneGrid,
+            grid=tuneGrid,
             metrics=metric_set(accuracy))
 
 ## Find best tuning parameters
-bestTune <- tuned_nn  %>%
+bestTune <- tuned_bt  %>%
   select_best("accuracy")
 
 # Finalize Workflow
-final_wf <- nn_wf %>%
+final_wf <- bt_wf %>%
   finalize_workflow(bestTune) %>%
   fit(data=trainSet)
 
@@ -148,14 +211,54 @@ results <- testSet %>%
   select(id, type)
 
 # get csv file
-vroom_write(results, 'GGGPredsnn.csv', delim = ",")
+vroom_write(results, 'GGGPredsbt.csv', delim = ",")
 
-# Plot graph
-graph <- tuned_nn %>% collect_metrics() %>%
-  filter(.metric=="accuracy") %>%
-  ggplot(aes(x=hidden_units, y=mean)) + geom_line()
-graph
-save(file="./MyFile.RData", list=c("predictions", "tuned_nn"))
-
+# # BART -------------------------------------------------------------------
+# bt_recipe <- recipe(type~., data=trainSet) %>%
+#   step_dummy(all_nominal_predictors())
+# 
+# bart_model <- bart(trees=tune()) %>% # BART figures out depth and learn_rate9
+#   set_engine("dbarts") %>% # might need to install10
+#   set_mode("classification")
+# 
+# 
+# # Workflow
+# bt_wf <- workflow() %>%
+#   add_recipe(bt_recipe) %>%
+#   add_model(bart_model)
+# 
+# # Tune
+# tuneGrid <- grid_regular(trees(),
+#                          levels = 5)
+# 
+# ## Set up K-fold CV
+# folds <- vfold_cv(trainSet, v = 5, repeats=1)
+# 
+# # Cross Validation
+# tuned_bt <- bt_wf %>%
+#   tune_grid(resamples=folds,
+#             grid=tuneGrid,
+#             metrics=metric_set(accuracy))
+# 
+# ## Find best tuning parameters
+# bestTune <- tuned_bt  %>%
+#   select_best("accuracy")
+# 
+# # Finalize Workflow
+# final_wf <- bt_wf %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=trainSet)
+# 
+# ## Predict
+# predictions <- final_wf %>%
+#   predict(new_data = testSet, type = "class")
+# 
+# # Format table
+# testSet$type <- predictions$.pred_class
+# results <- testSet %>%
+#   select(id, type)
+# 
+# # get csv file
+# vroom_write(results, 'GGGPredsbart.csv', delim = ",")
 
 # stopCluster(cl)
